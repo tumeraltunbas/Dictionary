844ff1e7a626c1880dee48fc1e03756e10052e01
tumeraltunbas <76481188+tumeraltunbas@users.noreply.github.com>
2021-01-22 18:15:12 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<TypeKey, TypeValue>
+    {
+        TypeKey[] keys;
+        TypeValue[] values;
+        public MyDictionary()
+        {
+            keys = new TypeKey[0];
+            values = new TypeValue[0];
+        }
+        public void Add(TypeKey key,TypeValue value)
+        {
+            TypeKey[] tempKeys = keys;
+            TypeValue[] tempValues = values;
+            keys = new TypeKey[keys.Length + 1];
+            values = new TypeValue[values.Length + 1];
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+    }
+    
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> numberName = new MyDictionary<int, string>();
+            numberName.Add(22, "Tumer");
+            numberName.Add(9, "Güven");
+            numberName.Add(30, "Ersin");
+        }
+    }
+}
